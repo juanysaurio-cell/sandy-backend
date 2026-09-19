2cd9752d0166581c70ea191615e448ca5e170849
juanysaurio-cell <juanysaurio@gmail.com>
2026-09-19 10:41:33 -0500

    Configurar variables de entorno para la base de datos

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const conexion = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+const db = mysql.createConnection({
+  host: process.env.DB_HOST || 'bkktmc4yvuhaclns8uwh-mysql.services.clever-cloud.com',
+  user: process.env.DB_USER || 'uialrcsnm98vb269',
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME || 'bkktmc4yvuhaclns8uwh',
+  port: process.env.DB_PORT || 3306
 });
 
 conexion.connect((error) => {
